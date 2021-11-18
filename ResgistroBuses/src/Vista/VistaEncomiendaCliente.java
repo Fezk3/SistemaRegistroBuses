45162c68c5d249093e4b83f5dba6931d251878a5
Victor Segura <seguravictor57@gmail.com>
2021-11-17 18:34:54 -0600

    encomienda cliente

--- ResgistroBuses/src/Vista/VistaEncomiendaCliente.java
+++ ResgistroBuses/src/Vista/VistaEncomiendaCliente.java
@@ -91,17 +91,7 @@ public class VistaEncomiendaCliente extends javax.swing.JFrame {
             }
         });
 
-        tablaEncomi.setModel(new javax.swing.table.DefaultTableModel(
-            new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
-            },
-            new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
-            }
-        ));
+        tablaEncomi.setModel(modelo);
         jScrollPane1.setViewportView(tablaEncomi);
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
